a0d4584bd2c9e11a3623bfcc89da2581f8202db8
kaben <KisingaCip@users.noreply.github.com>
2018-03-23 17:49:25 +0300

    split files and added global file

---
+++ global.R
@@ -0,0 +1,12 @@
+
+event_options <- c(
+  "Agricultural Sector Productivity & Food Security (ASP&FS)" = 1,
+  "Maternal/Child Health & Nutrition (H&N)" = 2,
+  "Business Development Services (BDS)" = 3
+)
+quarter_options <- c("2016.2", "2016.3", "2016.4", "2017.1", "2017.2", "2017.3", "2017.4", "2018.1", "2018.2", "2018.3")
+
+county_options <- c("Meru" = 13, "Uasin Gishu" = 21, "Nandi" = 15, "Marakwet" = 4, "Bomet" = 1)
+
+indicators <- c("EG. 11-6", "HL.9-2", "HL.9-4", "HL.9.1-c", "EG.3-6.-7.-8", "EG.3.2-18", "EG.3.2-17", "EG.3.2-1", "EG.3.2-5", "EG.3-1", "EG.3.1-12")
+
--- ui.R
+++ ui.R
@@ -1,16 +1,6 @@
+
 library(pacman)
 p_load(shiny, shinymaterial, shinydashboard, shinyWidgets)
-
-event_options <- c(
-  "Agricultural Sector Productivity & Food Security (ASP&FS)" = 1,
-  "Maternal/Child Health & Nutrition (H&N)" = 2,
-  "Business Development Services (BDS)" = 3
-)
-quarter_options <- c("2016.2", "2016.3", "2016.4", "2017.1", "2017.2", "2017.3", "2017.4", "2018.1", "2018.2", "2018.3")
-
-county_options <- c("Meru" = 13, "Uasin Gishu" = 21, "Nandi" = 15, "Marakwet" = 4, "Bomet" = 1)
-
-indicators <- c("EG. 11-6", "HL.9-2", "HL.9-4", "HL.9.1-c", "EG.3-6.-7.-8", "EG.3.2-18", "EG.3.2-17", "EG.3.2-1", "EG.3.2-5", "EG.3-1", "EG.3.1-12")
 
 ui <- material_page(
   title = "Draft Dash",
@@ -18,7 +8,7 @@ ui <- material_page(
   nav_bar_color = "#3e2723 brown darken-3",
   nav_bar_fixed = F,
   include_fonts = T,
-
+  
   # side bar panel
   material_side_nav(
     fixed = TRUE,
@@ -54,16 +44,16 @@ ui <- material_page(
       material_column(
         width = 3,
         awesomeCheckboxGroup("county", "County",
-          selected = c(13, 21, 15, 4, 1),
-          choices = county_options,
-          inline = T, status = "success"
+                             selected = c(13, 21, 15, 4, 1),
+                             choices = county_options,
+                             inline = T, status = "success"
         )
       ),
       material_column(
         width = 4,
         selectInput("quarters", "Select Quarter:",
-          choices = c(Choose = "", quarter_options),
-          multiple = T, selected = quarter_options
+                    choices = c(Choose = "", quarter_options),
+                    multiple = T, selected = quarter_options
         )
       )
     ),
@@ -71,7 +61,7 @@ ui <- material_page(
       material_column(
         offset = 1,
         width = 4,
-
+        
         awesomeRadio(
           inputId = "indicator",
           label = "Idicators",
@@ -96,16 +86,9 @@ ui <- material_page(
         offset = 1,
         width = 6,
         plotOutput("genderplot"))
-      )
     )
   )
+)
 
 
-
-
-server <- function(input, output) {
-
-}
-
-
-shinyApp(ui, server)
+shinyUI(ui)
